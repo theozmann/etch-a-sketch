ab22aea007791b1a79e4e422e60f1ea42231ddb3
Austin Williams <ozmancoding@gmail.com>
2022-08-11 22:04:05 -0400

    add logic functionality to grid size slider

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-const grid = document.querySelector(".grid");
-let gridSize = document.getElementById("grid-slider").value;
-const gridElement = document.querySelector(".grid-element");
+const DEFAULT_COLOR = '#333'
+const DEFAULT_SIZE = 16;
 
-function createGrid(gridSize) {
-  grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-  grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-  for (i = 0; i < gridSize * gridSize; i++) {
+let currentColor = DEFAULT_COLOR;
+let currentSize = DEFAULT_SIZE;
+
+const grid = document.querySelector(".grid");
+const gridElement = document.querySelector(".grid-element");
+const blackBtn = document.querySelector(".black-btn");
+const gradualBtn = document.querySelector(".gradual-btn");
+const rainbowBtn = document.querySelector(".rainbow-btn");
+const secretBtn = document.querySelector(".secret-btn");
+
+let gridSizeDisplay = document.querySelector(".grid-size-display");
+let gridSize = document.getElementById("grid-slider");
+
+gridSizeDisplay.textContent = gridSize.value;
+gridSize.oninput = function () {
+    gridSizeDisplay.textContent = this.value;
+    setCurrentSize(this.value);
+    clearGrid();
+    createGrid(currentSize);
+}
+
+
+function setCurrentColor(newColor) {
+    currentColor = newColor;
+}
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
+function clearGrid() {
+    grid.innerHTML = '';
+}
+
+
+function createGrid(currentSize) {
+  grid.style.gridTemplateColumns = `repeat(${currentSize}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${currentSize}, 1fr)`;
+  for (i = 0; i < currentSize * currentSize; i++) {
     const gridElement = document.createElement("div");
     gridElement.classList.add("grid-element");
     gridElement.addEventListener ('mouseover', () => {
@@ -15,6 +49,6 @@ function createGrid(gridSize) {
   }
 }
 
+createGrid(currentSize);
 
-createGrid(16);
 
